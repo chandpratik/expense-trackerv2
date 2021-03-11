169d688da897052bff279aae3c2ce9f2ddf8731c
pratikal1 <chandpratikal1@gmail.com>
2021-03-11 20:10:00 +0530

    Advanced expense tracker with voice input using speechly

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from '@material-ui/core';
+import useStyles from './Styles';
+
+import { ExpenseTrackerContext } from '../context/context';
+import Form from './Form/Form';
+import List from './List/List';
+import Infocard from '../Infocard';
+
+const Main = () => {
+  const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          Total Balance ${balance}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: '1.5em', marginTop: '20px' }}
+        >
+          <Infocard />
+        </Typography>
+        <Divider className={classes.divider} />
+        <Form />
+        {/* Form */}
+      </CardContent>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Main;
